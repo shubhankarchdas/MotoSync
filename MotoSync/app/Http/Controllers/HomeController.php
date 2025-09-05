9c1a9d67aeec6e45ce72713b9419aace2e4b8241
shubhankarchdas <shubhankarchdas@gmail.com>
2025-09-05 08:19:21 +0530

    Refactor HomeController to retrieve all cars and remove SoftDeletes from Car model

--- MotoSync/app/Http/Controllers/HomeController.php
+++ MotoSync/app/Http/Controllers/HomeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Car;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
+        // select all cars
+        $cars = Car::get();
+
+        //select published cars
+        // $cars = Car::where('published_at', '!=', null)->get();
+        dump($cars);
         return  view ('home.index');
+
     }
 }
