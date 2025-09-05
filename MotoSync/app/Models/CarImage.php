0aae5845e299ed14694372041eda2288d82d8e0d
shubhankarchdas <shubhankarchdas@gmail.com>
2025-09-05 10:57:40 +0530

    Add CarImage model with fillable attributes and disabled timestamps

--- MotoSync/app/Models/CarImage.php
+++ MotoSync/app/Models/CarImage.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CarImages extends Model
+class CarImage extends Model
 {
     use HasFactory;
 
